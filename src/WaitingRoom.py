df27071909681cd94c54f7933e10b63f338a51be
Agustin Contreras <contreras.agustin.1999@gmail.com>
2023-10-30 12:38:13 -0300

    Changes

--- src/WaitingRoom.py
+++ src/WaitingRoom.py
@@ -1,72 +1,62 @@
-import multiprocessing
-import time
-import tkinter as tk
-from tkinter import Frame, ttk, messagebox
-import threading
 import os
-import libs.fifo as lf
-import libs.patient as lp
+import tkinter as tk
 
-lista_strings = ["123","123123"]
+def recv_msg():
+    global window, listbox
+    fifo = "internal_msg"
+    with open(fifo, "r") as fifo:
+        datos = fifo.read()
+        listbox.insert(tk.END,datos)
+    window.after(1000,recv_msg)
+    window.update()
 
-class WaitingRoom:
-    def __init__(self, root):
-        self.root = root
-        root.title("Sala de espera")
-        root.configure(bg="#457B9D")
-        self.create_widgets()
+def send_msg(mensaje):
+    fifo = "internal_msg"
+    with open(fifo, "w") as fifo:
+        fifo.write(mensaje)
 
-    def create_widgets(self):
-        self.root.attributes('-zoomed', True)
-        self.frame = Frame(self.root)
-        self.frame.pack()
-        self.state = False
-        self.root.bind("<F11>", self.toggle_fullscreen)
-        self.root.bind("<Escape>", self.end_fullscreen)
-        frame = tk.Frame(self.root, background="#457B9D")
-        frame.pack()
-        listbox = tk.Listbox(self.root,width=45, height=10,font=("Helvetica", 50,"bold"))
-        listbox.place(x=10,y=10)
-        listbox.pack()
-        for item in lista_strings:
-            listbox.insert(tk.END, item)
+def process_interface():
+    global window, listbox
+    window = tk.Tk()
+    window.title("Sala de espera")
+    window.configure(bg="#457B9D")
+    window.attributes('-zoomed', True)
+    frame = tk.Frame(window, background="#457B9D")
+    frame.pack()
+    listbox = tk.Listbox(window,width=45, height=10,font=("Helvetica", 50,"bold"))
+    listbox.place(x=10,y=10)
+    listbox.pack()
+    window.after(1000,recv_msg)
+    window.mainloop()
 
-    def toggle_fullscreen(self, event=None):
-        self.state = not self.state 
-        self.tk.attributes("-fullscreen", self.state)
-        return "break"
-
-    def end_fullscreen(self, event=None):
-        self.state = False
-        self.tk.attributes("-fullscreen", False)
-        return "break"
-    
-def launch_graphic():
-    root = tk.Tk()
-    app = WaitingRoom(root)
-    root.mainloop()
-
-def waiting_msg():
+def process_logic():
     while True:
-        thread_logic=threading.Thread(target=process_msg())
-        thread_logic.daemon=True
-        thread_logic.start()
-        time.sleep(10)
-
-def process_msg():
-    print("procesando msg")
+        msg = input("Escribe un mensaje (o 'q' para salir): ")
+        if msg.lower() == 'q':
+            send_msg('q')  # Enviar un mensaje de salida
+            break
+        send_msg(msg)
 
 def main():
-    process_graphic=os.fork()
-    if(process_graphic==-1):
-        print("Error")
-        return -1
-    send, recive = multiprocessing.Pipe()
-    if(process_graphic==0):
-        launch_graphic()
-    else:
-        waiting_msg()
+    fifo = "internal_msg"
+    if not os.path.exists(fifo):
+        os.mkfifo(fifo)
+    pidL=os.fork()
+    if pidL==-1:
+        print("ERROR")
+        exit -1
+    if pidL == 0:
+        process_logic()
+    pidI = os.fork()
+    if pidI==-1:
+        print("ERROR")
+        exit -1
+    if pidI == 0:  # Proceso hijo para la interfaz gráfica
+        process_interface()
+    
+    os.wait()  # Esperar a que el proceso hijo termine
+    os.wait()  # Esperar a que el proceso hijo termine
+
 
 if __name__ == "__main__":
     main()
- 
